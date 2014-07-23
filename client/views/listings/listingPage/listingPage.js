11181ba9e74ad032190099768c9fcff8477cbfea
Noonan Jacob <noonanja@wharton.upenn.edu>
2014-07-23 00:03:05 -0700

    trying to do "load more"

--- client/views/listings/listingPage/listingPage.js
+++
@@ -1,9 +0,0 @@
-Template.listingPage.helpers({ 
-    postsWithRank: function() {
-	   Posts.find({category: this.title}).rewind();
-	   return Posts.find({category: this.title}).map(function(post, index, cursor) {
-		 post._rank = index;
-		 return post;
-	   });
-	}
-});
