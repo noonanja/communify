84b7975465e8b7cb8f09396b85f64c5d01204ef1
Greg Gilles <gillesg@wharton.upenn.edu>
2014-07-23 12:45:02 -0700

    some dropdowns and style for listings page

---
+++ client/views/listings/listingPage/leftBarListingPage.js
@@ -0,0 +1,14 @@
+Template.leftBarListingPage.helpers({
+	forSale: function() {
+		return Listings.find({category: 'forSale'});
+	},
+	jobs: function() {
+		return Listings.find({category: 'jobs'}); 
+	},
+	housing: function() {
+		return Listings.find({category: 'housing'}); 
+	},
+	community: function() {
+		return Listings.find({category: 'community'}); 
+	}
+});
