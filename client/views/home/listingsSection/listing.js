85c2f11f9d54bd908098cdbf87f78d6621b59205
Greg Gilles <gillesg@wharton.upenn.edu>
2014-07-23 17:11:03 -0700

    Revert 2e8d6a0..c837495
    This rolls back to commit 2e8d6a0b62fcce0b0f557265627ab3d986f0269d.

--- client/views/home/listingsSection/listing.js
+++
@@ -1,6 +0,0 @@
-// Template.listing.events({ 
-// 	'click #title': function(e) {
-// 		Session.set('category', $(e.target).text());
-// 		Router.go('listingPage', {category: this.title});
-// 	}
-// });
--- public/pages/pages.js
+++
@@ -1,6 +0,0 @@
-PostPaging = new Meteor.Pagination('posts',{
-    router: 'iron-router',
-    routerTemplate: 'postsList',
-    route: '/listings/:title',
-    perPage: 4,
-}) 
