390c72f858a98fe66580a06a5ae161ddaf4e674e
Greg Gilles <gillesg@wharton.upenn.edu>
2014-07-29 13:58:15 -0700

    new new na new

---
+++ client/views/application/listing.js
@@ -0,0 +1,6 @@
+// Template.listing.events({ 
+// 	'click #title': function(e) {
+// 		Session.set('category', $(e.target).text());
+// 		Router.go('listingPage', {category: this.title});
+// 	}
+// });
--- client/views/home/listingsSection/listing.js
+++
@@ -1,5 +0,0 @@
-Template.listing.events({ 
-	'click .listing': function(e) {
-		Session.set('limit', 10);
-	}
-});
