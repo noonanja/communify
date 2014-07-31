7b09866184a0409a7d4e7dfa717b276d4c2643f7
Noonan Jacob <noonanja@wharton.upenn.edu>
2014-07-30 19:15:36 -0700

    getting to end of day

--- client/views/listings/postSubmit/postSubmit.js
+++ client/views/listings/postSubmit/postSubmit.js
@@ -36,6 +36,22 @@ Template.postSubmit.events({
 
 });
 
-
+Template.postSubmit.helpers({
+  forSale1: function() {
+    return Listings.find({category: 'forSale'}, {limit: 8});
+  },
+  forSale2: function() {
+    return Listings.find({category: 'forSale'}, {skip: 8});
+  },
+  jobs: function() {
+    return Listings.find({category: 'jobs'}); 
+  },
+  housing: function() {
+    return Listings.find({category: 'housing'}); 
+  },
+  community: function() {
+    return Listings.find({category: 'community'}); 
+  }
+});
 
 
