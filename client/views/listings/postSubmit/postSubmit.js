426b8fcf936fcd1f5482c86ec4cdb7c7add32941
Noonan Jacob <noonanja@wharton.upenn.edu>
2014-08-01 19:46:27 -0700

    getting there on postContinue

--- client/views/listings/postSubmit/postSubmit.js
+++ client/views/listings/postSubmit/postSubmit.js
@@ -15,3 +15,37 @@ Template.postSubmit.helpers({
     return Listings.find({category: 'community'}); 
   }
 });
+
+Template.postSubmit.events({
+  'click #continue': function(e) {
+    e.preventDefault();
+    console.log($(e.target).find('[name=title]').val());
+    var post = {
+      // category: $(e.target).find('[name=category]').val(),
+      // title: $(e.target).find('[name=title]').val(),
+      // authors: $(e.target).find('[name=authors]').val(),
+      // emails: $(e.target).find('[name=emails]').val(),
+      // description: $(e.target).find('[name=description]').val(),
+      // price: $(e.target).find('[name=price]').val() this need to be a number
+      category: 'Animals',
+      title: 'Roommate wanted',
+      authors: 'Jake Noonan',
+      emails: 'noonanja@wharton.upenn.edu',
+      description: 'I want an animal for a Roommate!',
+      price: null
+    }
+
+    
+    Meteor.call('post', post, function(error, id) {
+      if (error) {
+        // display the error to the user
+        throwError(error.reason);
+        
+        if (error.error === 302)
+          Router.go('postPage', {_id: error.details})
+      } else {
+        Router.go('postContinue', {_id: id});
+      }
+    });
+  }
+});
