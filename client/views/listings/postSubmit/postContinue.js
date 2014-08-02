56091848c4c26a614af35e708cddae5a0a69c2a6
Noonan Jacob <noonanja@wharton.upenn.edu>
2014-08-01 18:55:57 -0700

    postContinue

--- client/views/listings/postSubmit/postContinue.js
+++ client/views/listings/postSubmit/postContinue.js
@@ -1,29 +1,4 @@
 Template.postContinue.events({
-  'submit form': function(e) {
-    e.preventDefault();
-    
-    var post = {
-      category: $(e.target).find('[name=category]').val(),
-      title: $(e.target).find('[name=title]').val(),
-      authors: $(e.target).find('[name=authors]').val(),
-      emails: $(e.target).find('[name=emails]').val(),
-      description: $(e.target).find('[name=description]').val(),
-      price: $(e.target).find('[name=price]').val(),
-    }
-
-    
-    Meteor.call('post', post, function(error, id) {
-      if (error) {
-        // display the error to the user
-        throwError(error.reason);
-        
-        if (error.error === 302)
-          Router.go('postPage', {_id: error.details})
-      } else {
-        Router.go('postPage', {_id: id});
-      }
-    });
-  },
   'click .del-file': function(e, t) {
     return myFiles.remove({
       _id: this._id
--- client/views/listings/postSubmit/postSubmit.js
+++ client/views/listings/postSubmit/postSubmit.js
@@ -16,3 +16,30 @@ Template.postSubmit.helpers({
   }
 });
 
+Template.postSubmit.events({
+  'click #continue': function(e) {
+    e.preventDefault();
+    
+    var post = {
+      category: $(e.target).find('[name=category]').val(),
+      title: $(e.target).find('[name=title]').val(),
+      authors: $(e.target).find('[name=authors]').val(),
+      emails: $(e.target).find('[name=emails]').val(),
+      description: $(e.target).find('[name=description]').val(),
+      price: $(e.target).find('[name=price]').val(),
+    }
+
+    
+    Meteor.call('post', post, function(error, id) {
+      if (error) {
+        // display the error to the user
+        throwError(error.reason);
+        
+        if (error.error === 302)
+          Router.go('postPage', {_id: error.details})
+      } else {
+        Router.go('postContinue', {_id: id});
+      }
+    });
+  }
+});
