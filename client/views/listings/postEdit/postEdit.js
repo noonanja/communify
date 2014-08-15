9638ea13fe3114598e1633b268177a7ed1381a5e
Noonan Jacob <noonanja@wharton.upenn.edu>
2014-08-14 22:13:34 -0700

    some sweet stylin

--- client/views/listings/postEdit/postEdit.js
+++ client/views/listings/postEdit/postEdit.js
@@ -1,21 +1,23 @@
 Template.postEdit.events({
-
-  'submit form': function(e) {
+  'click #updatePost': function(e) {
     e.preventDefault();
     
-    var currentPostId = this._id;
-    
     var postProperties = {
-      url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val()
+      currentPostId: this._id,
+      authors: $('#update').find('[name=authors]').val(),
+      title: $('#update').find('[name=title]').val(),
+      description: $('#update').find('[name=description]').val()
     }
     
-    Posts.update(currentPostId, {$set: postProperties}, function(error) {
+    Meteor.call('update', postProperties, function(error, id) {
       if (error) {
         // display the error to the user
         throwError(error.reason);
+        
+        if (error.error === 302)
+          Router.go('postPage', {_id: error.details})
       } else {
-        Router.go('postPage', {_id: currentPostId});
+        Router.go('postPage', {_id: id});
       }
     });
   },
--- collections/posts.js
+++ collections/posts.js
@@ -15,6 +15,16 @@ Posts.deny({
   }
 });
 
+var escape = function(s) {
+  return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+};
+
+Posts.search = function(term, options) {
+  var where = {title: {$regex: escape(term), $options: 'i'}};
+  var where2 = {description: {$regex: escape(term), $options: 'i'}};
+  // search the mongoDB with an OR clause
+  return Posts.find({$or: [where, where2]}, options);
+}
 
 Meteor.methods({
   post: function(postAttributes) {
@@ -63,6 +73,32 @@ Meteor.methods({
     
     return postId;
   },
+
+  update: function(postAttributes) {
+    var user = Meteor.user()
+    
+    // hopefully the user can edit a post if he's not logged in... but just in case
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to edit this listing");
+    
+    // ensure the post has a title
+    if (!postAttributes.title)
+      throw new Meteor.Error(422, 'Please name this listing');
+
+    //   // ensure the post has a category
+    // if (!postAttributes.category)
+    //   throw new Meteor.Error(422, 'Please choose a category for this listing');
+
+    if (!postAttributes.authors)
+      throw new Meteor.Error(422, 'Please give a name for this listing!');
+    
+    //  if (isNaN(postAttributes.price))
+    //   throw new Meteor.Error(422, 'Please only use integers for pricing');
+   
+   Posts.update(postAttributes.currentPostId, {$set: postAttributes});
+   return postAttributes.currentPostId;
+  },
+
   remove: function(postAttributes) {
       var userId = Meteor.user()._id;
       
